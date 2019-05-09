2480e63e661c86fdb03f9a4bcc9089d3ee08c768
Eduardo Leite <eduappsplay@gmail.com>
2019-05-09 15:33:23 -0300

    estruturando e estilizando o componente de cadastro de cidades

--- src/app/cadastro-cidade/cadastro-cidade.component.ts
+++ src/app/cadastro-cidade/cadastro-cidade.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Cidade } from 'src/model/cidade';
 
 @Component({
   selector: 'app-cadastro-cidade',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroCidadeComponent implements OnInit {
 
-  constructor() { }
+  cidades : Cidade[];
+  
+  constructor(private dataService: DataService) { }
+
+  public imgPlus = require("../../imagens/plus.svg");
+  public imgGarbage = require("../../imagens/garbage.svg");
+  public imgPencilStriped = require("../../imagens/pencil-striped.svg");
 
   ngOnInit() {
+    this.dataService.getCidades().subscribe(data => this.cidades = data);
   }
 
 }
