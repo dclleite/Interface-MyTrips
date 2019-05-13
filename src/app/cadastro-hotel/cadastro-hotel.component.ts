fdaeb00d0f526546ddee48ccedf601bc7e7b0671
Eduardo Leite <eduappsplay@gmail.com>
2019-05-13 11:09:01 -0300

    estilizando componente de cadastro de hotel

--- src/app/cadastro-hotel/cadastro-hotel.component.ts
+++ src/app/cadastro-hotel/cadastro-hotel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Hotel } from 'src/model/hotel';
+import { Cidade } from 'src/model/cidade';
 
 @Component({
   selector: 'app-cadastro-hotel',
@@ -10,13 +11,26 @@ import { Hotel } from 'src/model/hotel';
 export class CadastroHotelComponent implements OnInit {
 
   hoteis : Hotel[];
+  cidades : Cidade[];
   public imgGarbage = require("../../imagens/garbage.svg");
   public imgPencilStriped = require("../../imagens/pencil-striped.svg");
+  public imgPlaceholder = require("../../imagens/placeholder.svg");
   
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.dataService.geHoteis().subscribe(data => this.hoteis = data);
+    this.dataService.getCidade().subscribe(data => this.cidades = data);
+  }
+
+  deletarhotel(id){
+    this.dataService.deleteHotel(id).subscribe(data => {
+      console.log(data);
+      this.ngOnInit();
+    })
+
+    
+
   }
 
 }
